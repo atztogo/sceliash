0e1d7b6f4250bc29534db07d4d04ce9685d25816
Atsushi Togo <atz.togo@gmail.com>
2025-03-10 14:25:54 +0900

    Refactor Eliashberg equation solver

--- src/sceliash/eliashberg_eqn.py
+++ src/sceliash/eliashberg_eqn.py
@@ -2,16 +2,12 @@
 
 from __future__ import annotations
 
+from typing import Optional
+
 import numpy as np
-from phonopy.units import Hbar, Kb, THzToEv
+from phonopy.units import Kb, THzToEv
 
 from .eliashberg_func import EliashbergFunction
-
-
-def plot_lambda_function(vaspout_h5: str):
-    """Plot lambda function."""
-    ee = EliashbergEquation(EliashbergFunction(h5_filename=vaspout_h5).run()).run()
-    ee.plot_lambda_function()
 
 
 class EliashbergEquation:
@@ -20,16 +16,13 @@ class EliashbergEquation:
     Attributes
     ----------
     lambda_function : np.ndarray
-        Electron-phonon coupling function. shape=(ispin, itemp, n - n'), where
-        -2N <= n - n' <= 2N, and stored in range(0, 2N+2).
+        Electron-phonon coupling function. shape=(itemp, n - n'), where -N <= n
+        <= N, -2N <= n - n' <= 2N, and so values corersponding to n-n' are
+        stored in range(0, 2N+2).
 
     """
 
-    def __init__(
-        self,
-        eliashberg_func: EliashbergFunction,
-        mu_star: float = 0.1 / THzToEv * 2 * np.pi,
-    ):
+    def __init__(self, eliashberg_func: EliashbergFunction, mu_star: float = 0.1):
         """Initialize Eliashberg function class.
 
         Parameters
@@ -50,74 +43,104 @@ class EliashbergEquation:
 
     def run(self) -> "EliashbergEquation":
         """Run Eliashberg equation."""
-        self._lambda_funcs, self._temps = self._run_lambda_function()
+        self._lambda_funcs = self._run_lambda_function()
         return self
 
     def _run_lambda_function(self) -> list[np.ndarray]:
-        r"""Compute lambda function.
+        """Compute lambda function.
 
         Returns
         -------
         lambda_funcs : list[np.ndarray]
-            List of lambda functions. list[(ispin, 2N+1, 2N+1)]
+            List of lambda functions. list[np.ndarray((2N+1, 2N+1))]
 
         """
         delta_f = self._ef.freq_points[1] - self._ef.freq_points[0]
         indices = np.where(self._ef.freq_points > 1e-5)[0]
-        fpts = self._ef.freq_points[indices]
+        fpts = self._ef.freq_points[indices]  # (freq_points',)
+        fpts2 = fpts**2
 
         omega_c = np.max(self._ef.frequencies) * THzToEv / (2 * np.pi) * 10
-        max_n = np.rint((omega_c / (Kb * self._ef.temps) - np.pi) / (2 * np.pi)).astype(
+        max_n = np.ceil((omega_c / (Kb * self._ef.temps) - np.pi) / (2 * np.pi)).astype(
             int
         )
-        temps = []
         lambda_funcs = []
-        for i, temp in enumerate(self._ef.temps):
-            if max_n[i] < 50:
-                continue
-            else:
-                temps.append(temp)
-            coef = np.pi * 2 / (Hbar / (Kb * temp))
-            N_p = 2 * max_n[i] + 1
-            lambda_func = np.zeros((self._ef.a2f.shape[0], N_p, N_p), dtype="double")
-            for j, a2f_spin in enumerate(self._ef.a2f[:, i, indices]):
-                for n1, n2 in np.ndindex((N_p, N_p)):
-                    print(j, temp, n1, n2)
-                    lambda_func[j, n1, n2] = (
-                        a2f_spin
-                        * delta_f
-                        * 2
-                        * fpts
-                        / ((coef * (n1 - n2)) ** 2 + fpts**2)
-                    ).sum()
-                lambda_funcs.append(lambda_func)
+        for i_temp, temp in enumerate(self._ef.temps):
+            coef = (2 * np.pi) ** 2 * (Kb * temp) / THzToEv
+            N_p = 2 * max_n[i_temp] + 1
+            lambda_func = np.zeros(N_p * N_p, dtype="double")
+            n_vals = np.arange(N_p) - max_n[i_temp]
+            omega_n = coef * n_vals
+            a2f_2omega = (
+                2 * self._ef.a2f[:, i_temp, indices].sum(axis=0) * delta_f * fpts
+            )  # (freq_points',)
+            w1_w2_2 = np.subtract.outer(omega_n, omega_n) ** 2
+            lambda_func = (a2f_2omega / np.add.outer(w1_w2_2, fpts2)).sum(axis=-1)
+            lambda_funcs.append(lambda_func.reshape(N_p, N_p))
 
-        return lambda_funcs, temps
+        return lambda_funcs
 
-    def plot_lambda_function(self, is_log_scale: bool = True):
+    def run_equation(self) -> np.ndarray:
+        """Solve Eliashberg equation.
+
+        Returns
+        -------
+        max_eigs : np.ndarray
+            Maximum eigenvalues of the Eliashberg equation.
+
+        """
+        max_eigs = np.zeros(len(self._ef.temps))
+        for i, lambda_func in enumerate(self._lambda_funcs):
+            gamma_func = lambda_func.copy() - self._mu_star
+            N_p = lambda_func.shape[0]
+            max_n = (N_p - 1) // 2
+            n_vals = np.arange(N_p) - max_n
+            p_vals = 2 * n_vals + 1
+            v_s = np.sign(p_vals)
+            gamma_func[np.diag_indices_from(gamma_func)] -= v_s * (lambda_func @ v_s)
+            v_a = np.abs(p_vals)
+            gamma_func[:, :] /= np.sqrt(np.outer(v_a, v_a))
+            eigvals, _ = np.linalg.eigh(gamma_func)
+            max_eigs[i] = np.max(eigvals)
+
+        return max_eigs
+
+    def plot_lambda_function(
+        self, temperature: Optional[float] = None, is_log_scale: bool = False
+    ):
         """Plot lambda function."""
         import matplotlib.pyplot as plt
 
         plt.figure()
 
-        for i, temp in enumerate(self._temps):
-            for j, lambda_func_spin in enumerate(self._lambda_funcs[i]):
+        for i_temp, temp in enumerate(self._temps):
+            if temperature is not None and abs(temp - temperature) > 1e-5:
+                continue
+            for i_spin, lambda_func_spin in enumerate(self._lambda_funcs[i_temp]):
                 N_p = lambda_func_spin.shape[0]
-                lambda_func = np.zeros(N_p * 2 + 1, dtype="double")
-                for n1, n2 in np.ndindex(lambda_func_spin.shape):
-                    if is_log_scale:
-                        lambda_func[n1 - n2 + N_p] = lambda_func_spin[n1, n2]
-                    else:
-                        lambda_func[n1 - n2 + N_p] = 1 / lambda_func_spin[n1, n2]
+                max_n = (N_p - 1) // 2  # N
+                n_vals = np.arange(N_p) - max_n
+                lambda_func = np.zeros(4 * max_n + 1, dtype="double")
+                for j, n1 in enumerate(n_vals):  # -N .. N
+                    for k, n2 in enumerate(n_vals):  # -N .. N
+                        if is_log_scale:
+                            lambda_func[n1 - n2 + 2 * max_n] = lambda_func_spin[j, k]
+                        else:
+                            lambda_func[n1 - n2 + 2 * max_n] = (
+                                1 / lambda_func_spin[j, k]
+                            )
                 if is_log_scale:
                     plt.yscale("log")
                 plt.plot(
-                    np.arange(-N_p, N_p + 1),
+                    np.arange(-2 * max_n, 2 * max_n + 1),
                     lambda_func,
-                    label=f"spin={j}, temp={temp}",
+                    label=f"spin={i_spin}, temp={temp}",
                 )
 
         plt.xlabel("n - n'")
-        plt.ylabel("lambda function")
+        if is_log_scale:
+            plt.ylabel("lambda function")
+        else:
+            plt.ylabel("1/lambda function")
         plt.legend()
         plt.show()
